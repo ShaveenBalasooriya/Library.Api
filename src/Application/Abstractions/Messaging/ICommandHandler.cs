10dab76e1a3c43fafabac2d5d80568e27d5cf263
Shaveen Balasooriya <shaveen.balasooriya@ascentic.se>
2026-08-28 00:17:22 +0530

    [refactor]: Base Implementation of Application Layer
    Created the base implementation of the application layer by creating the generics for the Commands and Queries, and the Handlers. This make it easier to add stuff like Validation, and logging down the line.
    
    Signed-off-by: Shaveen Balasooriya <shaveen.balasooriya@ascentic.se>

---
+++ src/Application/Abstractions/Messaging/ICommandHandler.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Shared;
+using MediatR;
+
+namespace Application.Abstractions.Messaging;
+
+public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, Result<TResponse>>
+    where TCommand : ICommand<TResponse>
+{
+}
