1a681199165701198ee0f653154312ddb15c1d56
Shaveen Balasooriya <shaveen.balasooriya@ascentic.se>
2026-08-29 18:28:57 +0530

    [refactor]: Init Infrastructure Layer
    Added the base implementation of the infrastructure layer, with the package installation, Db context and Unit of work setup. Place holder for the DB string in the configurations.
    
    Signed-off-by: Shaveen Balasooriya <shaveen.balasooriya@ascentic.se>

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -1,11 +1,20 @@
+using Application.Abstractions.Data;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddDbContext<LibraryDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("Database")));
+
+        services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<LibraryDbContext>());
+
         return services;
     }
 }
--- src/Library.Api/Program.cs
+++ src/Library.Api/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
     .AddApplication()
-    .AddInfrastructure()
+    .AddInfrastructure(builder.Configuration)
     .AddPresentation();
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
