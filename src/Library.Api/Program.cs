cf9c826bdbd324daa08311b47c1bac553164f084
Shaveen Balasooriya <shaveen.balasooriya@ascentic.se>
2026-08-29 11:58:06 +0530

    [refactor]: Re-created the Library.Api
    Previously the wrong project was created for the Library.Api, now corrected add the correct ASP.NET Core WebAPI project.
    This inturn added the previously missing Program.cs file as well.
    Added Docker support directly through the VS setup.
    With the updated Container support Package added.
    
    Signed-off-by: Shaveen Balasooriya <shaveen.balasooriya@ascentic.se>

---
+++ src/Library.Api/Program.cs
@@ -0,0 +1,25 @@
+using Application;
+using Infrastructure;
+using Presentation;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddApplication()
+    .AddInfrastructure()
+    .AddPresentation();
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+//Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
